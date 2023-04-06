b8970655b67358a4c8850f2579d78159cdf9e20f
Hetal <hetu.chova2014@gmail.com>
2023-04-06 22:01:09 +0100

    For Loop Homework commit

--- src/Reverse_The_Number.java
+++ src/Reverse_The_Number.java
@@ -4,7 +4,9 @@ public class Reverse_The_Number {
     public static void main(String[] args) {
         ////Import Scanner to Input User value
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to be reversed:");
         int n= sc.nextInt();
+        System.out.println("Reversed number");
         int r=0;
         //executing the code
         for (int i =n; i !=0;i= i/10) {
